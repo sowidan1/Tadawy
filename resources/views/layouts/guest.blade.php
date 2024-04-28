a194570b1b24d5dcb34d4ffa0d2c8ff88fda9416
Osama Sowidan <124528987+sowidan1@users.noreply.github.com>
2024-04-28 14:59:23 +0300

    add server url to path

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,31 +11,10 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Vite-generated CSS -->
-
-    <link rel="stylesheet" href="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/fonts/boxicons.css') }}" />
-
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/css/theme-default.css') }}"
-        class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('https://tadawy-production.up.railway.app/assets/css/demo.css') }}" />
-    <link rel="stylesheet" href="{{ asset('https://tadawy-production.up.railway.app/dist/css/app.css') }}"/>
-
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-
-    <link rel="stylesheet" href="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
-
-    <!-- Page CSS -->
-
-    <!-- Helpers -->
-
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/js/config.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('https://tadawy-production.up.railway.app/css/app.css') }}">
 
     <!-- Vite-generated JavaScript -->
-    {{-- <script type="module" src="{{ asset('https://tadawy-production.up.railway.app/dist/js/app.js') }}"></script> --}}
+    <script type="module" src="{{ asset('https://tadawy-production.up.railway.app/js/app.js') }}"></script>
 </head>
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
@@ -49,28 +28,5 @@
             {{ $slot }}
         </div>
     </div>
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/js/menu.js') }}"></script>
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/vendor/js/helpers.js') }}"></script>
-    <script src="{{ asset('https://tadawy-production.up.railway.app/dist/js/app.js') }}"></script>
-
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
-
-    <!-- Main JS -->
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/js/main.js') }}"></script>
-
-    <!-- Page JS -->
-    <script src="{{ asset('https://tadawy-production.up.railway.app/assets/js/pages-account-settings-account.js') }}"></script>
-
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-
-
 </body>
 </html>
