4920444277ad266943d347613d8257f3e894fc85
Osama Sowidan <124528987+sowidan1@users.noreply.github.com>
2024-04-28 12:42:11 +0300

    add server url to path

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,15 +10,16 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://tadawy-production.up.railway.app/resources/css/app.css" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['https://tadawy-production.up.railway.app/resources/css/app.css','https://tadawy-production.up.railway.app/resources/js/app.js'])
+        {{-- @vite(['https://tadawy-production.up.railway.app/resources/css/app.css','https://tadawy-production.up.railway.app/resources/js/app.js']) --}}
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
-                     <img src="assets/img/icons/unicons/logo.png" class="w-40 h-40 fill-current text-gray-500" />
+                     <img src="{{ asset('https://tadawy-production.up.railway.app/assets/img/icons/unicons/logo.png') }}" class="w-40 h-40 fill-current text-gray-500" />
                 </a>
             </div>
 
