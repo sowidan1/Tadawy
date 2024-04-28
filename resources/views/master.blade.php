089e77b126b9e1cee6d7ebb986b5884773b803ff
Osama Sowidan <124528987+sowidan1@users.noreply.github.com>
2024-04-28 10:52:40 +0300

    edit app blade

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -17,7 +17,7 @@ use App\Models\Patient;
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/icons/unicons/logo.png') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/icons/unicons/logo.png') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -27,27 +27,27 @@ use App\Models\Patient;
         rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/fonts/boxicons.css') }}" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}"
+    <link rel="stylesheet" href="{{ asset('vendor/css/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('vendor/css/theme-default.css') }}"
         class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/demo.css') }}" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
 
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/libs/apex-charts/apex-charts.css') }}" />
 
     <!-- Page CSS -->
 
     <!-- Helpers -->
-    <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
+    <script src="{{ asset('vendor/js/helpers.js') }}"></script>
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{ asset('assets/js/config.js') }}"></script>
+    <script src="{{ asset('js/config.js') }}"></script>
 </head>
 
 <body>
@@ -91,7 +91,7 @@ use App\Models\Patient;
                         </span>
                         <span>
                             <div class="avatar flex-shrink-0">
-                                <img src="{{ asset('assets/img/icons/unicons/logo.png') }}" alt="LOGO"
+                                <img src="{{ asset('img/icons/unicons/logo.png') }}" alt="LOGO"
                                     class="rounded" />
                             </div>
                         </span>
@@ -181,7 +181,7 @@ use App\Models\Patient;
                                                 <div
                                                     class="card-title d-flex align-items-start justify-content-between">
                                                     <div class="avatar flex-shrink-0">
-                                                        <img src="{{ asset('assets/img/icons/unicons/calendar.png') }}"
+                                                        <img src="{{ asset('img/icons/unicons/calendar.png') }}"
                                                             alt="chart success" class="rounded" />
                                                     </div>
                                                 </div>
@@ -199,7 +199,7 @@ use App\Models\Patient;
                                                         <div
                                                             class="card-title d-flex align-items-start justify-content-between">
                                                             <div class="avatar flex-shrink-0">
-                                                                <img src="{{ asset('assets/img/icons/unicons/doctor.png') }}"
+                                                                <img src="{{ asset('img/icons/unicons/doctor.png') }}"
                                                                     alt="Credit Card" class="rounded" />
                                                             </div>
                                                             <div class="dropdown">
@@ -319,7 +319,7 @@ use App\Models\Patient;
                                             </li>
                                             <li class="d-flex mb-4 pb-1">
                                                 <div class="avatar flex-shrink-0 me-3">
-                                                    <img src="{{ asset('assets/img/icons/unicons/artificial-intelligence (1).png') }}"
+                                                    <img src="{{ asset('img/icons/unicons/artificial-intelligence (1).png') }}"
                                                         alt="User" class="rounded" />
                                                 </div>
                                                 <div
@@ -335,7 +335,7 @@ use App\Models\Patient;
                                             </li>
                                             <li class="d-flex mb-4 pb-1">
                                                 <div class="avatar flex-shrink-0 me-3">
-                                                    <img src="{{ asset('assets/img/icons/unicons/incubator - Copy.png') }}"
+                                                    <img src="{{ asset('img/icons/unicons/incubator - Copy.png') }}"
                                                         alt="User" class="rounded" />
                                                 </div>
                                                 <div
@@ -351,7 +351,7 @@ use App\Models\Patient;
                                             </li>
                                             <li class="d-flex">
                                                 <div class="avatar flex-shrink-0 me-3">
-                                                    <img src="{{ asset('assets/img/icons/unicons/health.png') }}"
+                                                    <img src="{{ asset('img/icons/unicons/health.png') }}"
                                                         alt="User" class="rounded" />
                                                 </div>
                                                 <div
@@ -392,7 +392,7 @@ use App\Models\Patient;
                                                 role="tabpanel">
                                                 <div class="d-flex p-4 pt-3">
                                                     <div class="avatar flex-shrink-0 me-3">
-                                                        <img src="{{ asset('assets/img/icons/unicons/wallet.png') }}"
+                                                        <img src="{{ asset('img/icons/unicons/wallet.png') }}"
                                                             alt="User" />
                                                     </div>
                                                     <div>
@@ -511,22 +511,22 @@ use App\Models\Patient;
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
-    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
-    <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
 
-    <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
+    <script src="{{ asset('vendor/js/menu.js') }}"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+    <script src="{{ asset('vendor/libs/apex-charts/apexcharts.js') }}"></script>
 
     <!-- Main JS -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 
     <!-- Page JS -->
-    <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script>
+    <script src="{{ asset('js/dashboards-analytics.js') }}"></script>
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
